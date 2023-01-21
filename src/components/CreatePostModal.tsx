54e5d47730bc52309d67f7da2c79492912b6d5f5
Yeray Lage <yeray.lage@abi.ai>
2023-01-21 20:31:05 +0100

    feat: upload files

--- src/components/CreatePostModal.tsx
+++ src/components/CreatePostModal.tsx
@@ -1,5 +1,7 @@
 import { useState, useEffect, useRef } from "react";
 import Webcam from "react-webcam";
+import { decode } from "base64-arraybuffer";
+import { createPost } from "../utils/supabase";
 
 export const CreatePostModal = () => {
   const webcamRef = useRef(null);
@@ -15,13 +17,19 @@ export const CreatePostModal = () => {
     setShowModal(lsAcceptedTerms && !lsImageTaken);
   }, []);
 
-  const handleTake = () => {
+  const handleTake = async () => {
     if (!webcamRef.current) return;
     const currentWebcam = webcamRef.current as any;
     const imageSrc = currentWebcam.getScreenshot();
     setImageSrc(imageSrc);
-    localStorage.setItem("imageTaken", "true");
-    localStorage.setItem("imageBase64", imageSrc);
+    // localStorage.setItem("imageTaken", "true");
+    // localStorage.setItem("imageBase64", imageSrc);
+
+    await createPost({
+      name: "test",
+      visible: true,
+      file: decode(imageSrc),
+    });
   };
 
   console.log({ imageSrc });
@@ -61,11 +69,11 @@ export const CreatePostModal = () => {
             width="24"
             height="24"
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="#151515"
             fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           >
             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
             <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2" />
---
+++ src/utils/supabase.ts
@@ -0,0 +1,63 @@
+import { createClient } from "@supabase/supabase-js";
+
+export const supabase = createClient(
+  "https://qnjsefzysabexpzkiqyr.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFuanNlZnp5c2FiZXhwemtpcXlyIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzQzMjEyNTYsImV4cCI6MTk4OTg5NzI1Nn0.yaBPDHTqUfirMCOjhRRXp5bqJ6AE3RJCIR6cpS99Qlg"
+);
+
+interface Post {
+  id: string;
+  created_at: string;
+  name: string;
+  visible: boolean;
+}
+
+export const createPost = async ({
+  name,
+  visible,
+  file,
+}: {
+  name: string;
+  visible: boolean;
+  file: ArrayBuffer;
+}) => {
+  const { data, error } = await supabase
+    .from("posts")
+    .insert({
+      name,
+      visible,
+    })
+    .select();
+
+  console.log({ data, error });
+
+  if (error || !data[0]) {
+    throw error;
+  }
+
+  const post = data[0] as Post;
+
+  await uploadPostFile({ id: post.id, file });
+
+  return post;
+};
+
+export const uploadPostFile = async ({
+  id,
+  file,
+}: {
+  id: string;
+  file: ArrayBuffer;
+}) => {
+  const { data, error } = await supabase.storage
+    .from("posts")
+    .upload(id, file, {
+      contentType: `image/jpeg`,
+    });
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+};
