7fa222acd6c34effd8cb819fdbb0dce00aeca16a
Yeray Lage <yeray.lage@abi.ai>
2023-01-23 23:49:59 +0100

    feat: feed layout and lazy loading

--- src/components/PostsViewer.tsx
+++ src/components/PostsViewer.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import type { Post } from '../utils/supabase'
 import html2canvas from 'html2canvas'
-import { OuRealLogo } from './OuRealLogo'
 
 const BASE_URL = 'https://qnjsefzysabexpzkiqyr.supabase.co/storage/v1/object/public/posts/'
 
@@ -42,6 +41,7 @@ export const PostsViewer = ({ posts }: { posts: Post[] }) => {
         title: 'Oureal - Entroido',
         files
       }
+
       if (navigator.canShare(shareData)) {
         try {
           await navigator.share(shareData)
@@ -62,18 +62,29 @@ export const PostsViewer = ({ posts }: { posts: Post[] }) => {
         <div className="w-full flex flex-col p-2 gap-2" key={post.id} id={post.id}>
           <div className="flex justify-between items-center px-2 gap-2">
             <span className="font-bold">{post.name}</span>
-            <span className="opacity-50 text-xs">
-              {new Intl.DateTimeFormat('es-ES', {
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit'
-              }).format(new Date(post.created_at))}
-            </span>
+            <div className="flex gap-2 items-center">
+              <span className="opacity-50 text-xs">
+                {new Intl.DateTimeFormat('es-ES', {
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit'
+                }).format(new Date(post.created_at))}
+              </span>
+              <button onClick={() => shareOnSocialMedia(post)}>
+                <img src="/share.svg" alt="Share" className="w-5 opacity-70" />
+              </button>
+            </div>
           </div>
 
           <div className="relative w-full">
-            <img className="w-full rounded" src={`${BASE_URL}${post.id}/${post.swap ? 'environment' : 'user'}.webp`} />
+            <img
+              className="rounded"
+              src={`${BASE_URL}${post.id}/${post.swap ? 'environment' : 'user'}.webp`}
+              loading="lazy"
+              width={1000}
+              height={200}
+            />
             <img
               onClick={() => {
                 setFormattedPosts((prev) =>
@@ -89,18 +100,17 @@ export const PostsViewer = ({ posts }: { posts: Post[] }) => {
                   })
                 )
               }}
-              className="absolute w-20 top-2 left-2 rounded border-2 border-black"
+              className="absolute top-2 left-2 rounded"
               src={`${BASE_URL}${post.id}/${!post.swap ? 'environment' : 'user'}.webp`}
+              loading="lazy"
+              width={100}
+              height={200}
             />
-          </div>
-
-          <div id="share-container" className="w-full flex items-center">
-            <button onClick={() => shareOnSocialMedia(post)}>share</button>
           </div>
         </div>
       ))}
 
-      <span className="py-6 text-center">¡No hay más posts!</span>
+      <span className="mt-2 mb-6 text-center opacity-50 text-xs">No hay más posts... Por ahora.</span>
     </div>
   )
 }
